ea7c4b90eba081f66995d7b196cb97306746d6d0
ygarciaar <ygarciaar@cesde.net>
2026-08-29 10:18:10 -0500

    actualizar repo

--- src/main/java/com/cesde/aivastudio/model/entity/Prompt.java
+++ src/main/java/com/cesde/aivastudio/model/entity/Prompt.java
@@ -1,5 +1,40 @@
 package com.cesde.aivastudio.model.entity;
 
-public class Prompt {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+import com.cesde.aivastudio.model.base.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "prompts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Prompt extends BaseEntity {
+
+    @Column(nullable = false, length = 500)
+    private String contenido;
+
+    @Column(length = 100)
+    private String categoria;
+
+    @ManyToMany(mappedBy = "prompts")
+    @Builder.Default
+    private List<Proyecto> proyectos = new ArrayList<>();
+
+    @OneToOne(mappedBy = "prompt")
+    private GeneracionVideo generacionVideo;
 }
